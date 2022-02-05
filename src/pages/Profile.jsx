dfc5fabfb0a11df95d7d4deda7f80e4b51b939df
¨Venturin¨ <diegoventurinn@gmail.com>
2022-02-05 17:40:50 -0300

    Implementado o requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from '../Components/Loading';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.getUser = this.searchUser.bind(this);
+
+    this.state = {
+      loading: true,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.searchUser();
+  }
+
+  async searchUser() {
+    const userAPI = await getUser();
+    this.setState({ loading: false, user: userAPI });
+  }
+
   render() {
+    const { loading, user } = this.state;
     return (
       <div data-testid="page-profile">
-        Profile
         <Header />
+        { loading ? (<Loading />) : (
+          <div>
+            <img data-testid="profile-image" src={ user.image } alt={ user.name } />
+            <p>Usuário: </p>
+            <p>{user.name}</p>
+            <p>E-mail: </p>
+            <p>{user.email}</p>
+            <p>Descrição: </p>
+            <p>{user.description}</p>
+            <Link to="/profile/edit"> Editar perfil </Link>
+          </div>)}
       </div>
     );
   }
